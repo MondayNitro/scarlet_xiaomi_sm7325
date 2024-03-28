51ad79032de43e6612237308eb6603b3239c8cbd
Ingo Molnar <mingo@kernel.org>
2024-03-28 12:07:48 +0100

    sched/balancing: Simplify the sg_status bitmask and use separate ->overloaded and ->overutilized flags
    SG_OVERLOADED and SG_OVERUTILIZED flags plus the sg_status bitmask are an
    unnecessary complication that only make the code harder to read and slower.
    
    We only ever set them separately:
    
     thule:~/tip> git grep SG_OVER kernel/sched/
     kernel/sched/fair.c:            set_rd_overutilized_status(rq->rd, SG_OVERUTILIZED);
     kernel/sched/fair.c:                    *sg_status |= SG_OVERLOADED;
     kernel/sched/fair.c:                    *sg_status |= SG_OVERUTILIZED;
     kernel/sched/fair.c:                            *sg_status |= SG_OVERLOADED;
     kernel/sched/fair.c:            set_rd_overloaded(env->dst_rq->rd, sg_status & SG_OVERLOADED);
     kernel/sched/fair.c:                                       sg_status & SG_OVERUTILIZED);
     kernel/sched/fair.c:    } else if (sg_status & SG_OVERUTILIZED) {
     kernel/sched/fair.c:            set_rd_overutilized_status(env->dst_rq->rd, SG_OVERUTILIZED);
     kernel/sched/sched.h:#define SG_OVERLOADED              0x1 /* More than one runnable task on a CPU. */
     kernel/sched/sched.h:#define SG_OVERUTILIZED            0x2 /* One or more CPUs are over-utilized. */
     kernel/sched/sched.h:           set_rd_overloaded(rq->rd, SG_OVERLOADED);
    
    And use them separately, which results in suboptimal code:
    
                    /* update overload indicator if we are at root domain */
                    set_rd_overloaded(env->dst_rq->rd, sg_status & SG_OVERLOADED);
    
                    /* Update over-utilization (tipping point, U >= 0) indicator */
                    set_rd_overutilized_status(env->dst_rq->rd,
    
    Introduce separate sg_overloaded and sg_overutilized flags in update_sd_lb_stats()
    and its lower level functions, and change all of them to 'bool'.
    
    Remove the now unused SG_OVERLOADED and SG_OVERUTILIZED flags.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Tested-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/ZgVPhODZ8/nbsqbP@gmail.com

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -670,7 +670,7 @@ struct rt_rq {
 #ifdef CONFIG_SMP
 	unsigned int		rt_nr_migratory;
 	unsigned int		rt_nr_total;
-	int			overloaded;
+	bool			overloaded;
 	struct plist_head	pushable_tasks;
 
 #endif /* CONFIG_SMP */
@@ -715,7 +715,7 @@ struct dl_rq {
 	} earliest_dl;
 
 	unsigned int		dl_nr_migratory;
-	int			overloaded;
+	bool			overloaded;
 
 	/*
 	 * Tasks on this rq that can be pushed away. They are kept in
@@ -822,10 +822,6 @@ struct max_cpu_capacity {
 	int cpu;
 };
 
-/* Scheduling group status flags */
-#define SG_OVERLOADED		0x1 /* More than one runnable task on a CPU. */
-#define SG_OVERUTILIZED		0x2 /* One or more CPUs are over-utilized. */
-
 /*
  * We add the notion of a root-domain which will be used to define per-domain
  * variables. Each exclusive cpuset essentially defines an island domain by
@@ -846,10 +842,10 @@ struct root_domain {
 	 * - More than one runnable task
 	 * - Running task is misfit
 	 */
-	int			overloaded;
+	bool			overloaded;
 
 	/* Indicate one or more cpus over-utilized (tipping point) */
-	int			overutilized;
+	bool			overutilized;
 
 	/*
 	 * The bit corresponding to a CPU gets set here if such CPU has more
@@ -2436,9 +2432,8 @@ static inline void add_nr_running(struct rq *rq, unsigned count)
 	rq->nr_running = prev_nr + count;
 
 #ifdef CONFIG_SMP
-	if (prev_nr < 2 && rq->nr_running >= 2) {
-		set_rd_overloaded(rq->rd, SG_OVERLOADED);
-	}
+	if (prev_nr < 2 && rq->nr_running >= 2)
+		set_rd_overloaded(rq->rd, 1);
 #endif
 
 	sched_update_tick_dependency(rq);
