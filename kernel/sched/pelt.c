MondayNitro/scarlet_xiaomi_sm7325

commit 4379d56ad8fd46782386cbeded5cef82b0f3a40e

sched: speed up pelt util increase
[root cause  ] pelt will decay the old load
[changes     ] if the task is running, accumulate it's load without decay
[side effects] no
[self test   ]:yes
[download normally]:yes
[power on/off normally]:yes
[do common repository/branch inspection]:yes
[is there dependence]:no
[confirm dependent commit]:yes
[board]:sharkl5pro
[test case]:no
[reviewers   ] vincent.wang ke.wang
[change_type ] bringup --> sprd_chip_support
[tag_product ] common

1. when use eas, if the task is running, accumulate it's load without decay;
2. trigger the cfs_rq_util_change ignore the decay;
3. change ewma's weight of new samples to 1/32 from 1/4

Bug: 1444927
Change-Id: I8c56a026650b9b58eb558635babd532d4727e788
Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

kernel/sched/pelt.c
@@ -110,6 +110,8 @@ accumulate_sum(u64 delta, struct sched_avg *sa,
 {
 	u32 contrib = (u32)delta; /* p == 0 -> delta < 1024 */
 	u64 periods;
+	u64 divider;
+	bool eas_enable = sched_energy_enabled();
 
 	delta += sa->period_contrib;
 	periods = delta / 1024; /* A period is 1024us (~1ms) */
@@ -118,38 +120,51 @@ accumulate_sum(u64 delta, struct sched_avg *sa,
 	 * Step 1: decay old *_sum if we crossed period boundaries.
 	 */
 	if (periods) {
-		sa->load_sum = decay_load(sa->load_sum, periods);
-		sa->runnable_sum =
-			decay_load(sa->runnable_sum, periods);
-		sa->util_sum = decay_load((u64)(sa->util_sum), periods);
+		if (running && eas_enable)
+			delta %= 1024;
+		else {
+			sa->load_sum = decay_load(sa->load_sum, periods);
+			sa->runnable_sum =
+				decay_load(sa->runnable_sum, periods);
+			sa->util_sum = decay_load((u64)(sa->util_sum), periods);
 
-		/*
-		 * Step 2
-		 */
-		delta %= 1024;
-		if (load) {
 			/*
-			 * This relies on the:
-			 *
-			 * if (!load)
-			 *	runnable = running = 0;
-			 *
-			 * clause from ___update_load_sum(); this results in
-			 * the below usage of @contrib to disappear entirely,
-			 * so no point in calculating it.
+			 * Step 2
 			 */
-			contrib = __accumulate_pelt_segments(periods,
-					1024 - sa->period_contrib, delta);
+			delta %= 1024;
+			if (load) {
+				/*
+				 * This relies on the:
+				 *
+				 * if (!load)
+				 *	runnable = running = 0;
+				 *
+				 * clause from ___update_load_sum(); this results in
+				 * the below usage of @contrib to disappear entirely,
+				 * so no point in calculating it.
+				 */
+				contrib = __accumulate_pelt_segments(periods,
+						1024 - sa->period_contrib, delta);
+			}
 		}
 	}
 	sa->period_contrib = delta;
+	divider = LOAD_AVG_MAX - 1024 + sa->period_contrib;
 
-	if (load)
+	if (load) {
 		sa->load_sum += load * contrib;
-	if (runnable)
+		sa->load_sum = min_t(u64, sa->load_sum, divider * load);
+	}
+
+	if (runnable) {
 		sa->runnable_sum += runnable * contrib << SCHED_CAPACITY_SHIFT;
-	if (running)
+		sa->runnable_sum = min_t(u64, sa->runnable_sum, divider * runnable);
+	}
+
+	if (running) {
 		sa->util_sum += contrib << SCHED_CAPACITY_SHIFT;
+		sa->util_sum = min_t(u64, sa->util_sum, divider << SCHED_CAPACITY_SHIFT);
+	}
 
 	return periods;
 }

include/linux/sched.h
@@ -356,7 +356,7 @@ struct load_weight {
 struct util_est {
 	unsigned int			enqueued;
 	unsigned int			ewma;
-#define UTIL_EST_WEIGHT_SHIFT		2
+#define UTIL_EST_WEIGHT_SHIFT		5
 #define UTIL_AVG_UNCHANGED		0x80000000
 } __attribute__((__aligned__(sizeof(u64))));
 

kernel/sched/fair.c
@@ -4333,11 +4333,10 @@ static inline void update_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *s
 		detach_entity_load_avg(cfs_rq, se);
 		update_tg_load_avg(cfs_rq);
 	} else if (decayed) {
-		cfs_rq_util_change(cfs_rq, 0);
-
 		if (flags & UPDATE_TG)
 			update_tg_load_avg(cfs_rq);
 	}
+	cfs_rq_util_change(cfs_rq, 0);
 }
 
 /*

commit ccd85dad87d37430f9f651abf55f57afea203ca7

sched/pelt: Fix runnable_sum clamp units mismatch
runnable_sum stores capacity-shifted values and is currently lacking
SCHED_CAPACITY_SHIFT in its overflow clamp. So, the unshifted limit artificially
caps utilization ~1024x too early, negating the PELT speedup done by commit
4379d56ad8f ("sched: speed up pelt util increase"). Fix the incorrect
cherry-pick resolution.

Fixes: 4379d56ad8f ("sched: speed up pelt util increase")
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

kernel/sched/pelt.c
@@ -158,7 +158,8 @@ accumulate_sum(u64 delta, struct sched_avg *sa,
 
 	if (runnable) {
 		sa->runnable_sum += runnable * contrib << SCHED_CAPACITY_SHIFT;
-		sa->runnable_sum = min_t(u64, sa->runnable_sum, divider * runnable);
+		sa->runnable_sum = min_t(u64, sa->runnable_sum,
+					 divider * runnable << SCHED_CAPACITY_SHIFT);
 	}
 
 	if (running) {